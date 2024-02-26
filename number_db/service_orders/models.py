7e45e559991521274cab5c5da9318aeeaf3e2878
Vector0108 <comsuper0030@gmail.com>
2024-02-26 16:00:51 -0500

    [Added] status field in service order page

--- number_db/service_orders/models.py
+++ number_db/service_orders/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-from assist_dids.models import *
-from customers.models import *
+from django.utils.translation import gettext_lazy as _
 import datetime
 
+class StatusValues(models.IntegerChoices):
+    CREATED = 0, _('Created')
+    UPDATED = 1, _('Updated')
+    DELETED = 2, _('Deleted')
 
 class Service_Order(models.Model):
     username = models.CharField(max_length=200, unique=True, null=False, blank=False)
@@ -12,5 +15,6 @@ class Service_Order(models.Model):
     requested_port_date = models.DateField('%m/%d/%Y', null=True, blank=True)
     e911_number = models.BigIntegerField(null=True, blank=True)
     e911_address = models.CharField(max_length=255, null=True, blank=True)
+    status = models.IntegerField(choices=StatusValues.choices, default=0, null=False, blank=False)
     created_at = models.DateTimeField(default=datetime.datetime.now, editable=False)
     updated_at = models.DateTimeField(default=datetime.datetime.now)
--- number_db/service_orders/views.py
+++ number_db/service_orders/views.py
@@ -10,6 +10,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.hashers import make_password
 import pandas as pd
 from operator import or_
+import smtplib
 
 # Create your views here.
 
@@ -20,6 +21,23 @@ def parse_date(date_string):
         return datetime.datetime.strptime(date_string, '%m/%d/%Y').date() if date_string else None
 
 default_data_header = ['username', 'email', 'number', 'texting', 'e911_number', 'e911_address']
+
+def switchStatus(value):
+    if value == 0:
+        return 'Created'
+    elif value == 1:
+        return 'Updated'
+    elif value == 2:
+        return 'Deleted'
+    elif value.lower() == 'created':
+        return 0
+    elif value.lower() == 'updated':
+        return 1
+    elif value.lower() == 'deleted':
+        return 2
+    else:
+        return None
+
 
 @login_required
 def service_order_list(request):
@@ -33,13 +51,14 @@ def service_order_list(request):
         service_orders = Service_Order.objects.filter(q_objects)
 
         for item in service_orders:
-            item.username =  "" if(item.username == None) else item.username
-            item.email =  "" if(item.email == None) else item.email
-            item.requested_port_date =  "" if(item.requested_port_date == None) else item.requested_port_date.strftime('%Y-%m-%d')
-            item.number =  "" if(item.number == None) else item.number
-            item.texting =  "" if(item.texting == None) else item.texting
-            item.e911_address =  "" if(item.e911_address == None) else item.e911_address
-            item.e911_number =  "" if(item.e911_number == None) else item.e911_number
+            item.username = "" if(item.username == None) else item.username
+            item.email = "" if(item.email == None) else item.email
+            item.requested_port_date = "" if(item.requested_port_date == None) else item.requested_port_date.strftime('%Y-%m-%d')
+            item.number = "" if(item.number == None) else item.number
+            item.texting = "" if(item.texting == None) else item.texting
+            item.status = switchStatus(item.status)
+            item.e911_address = "" if(item.e911_address == None) else item.e911_address
+            item.e911_number = "" if(item.e911_number == None) else item.e911_number
 
         size = request.GET.get('size', 10)
         page_number = request.GET.get('page')
@@ -51,13 +70,14 @@ def service_order_list(request):
         service_orders = Service_Order.objects.all()
         
         for item in service_orders:
-            item.username =  "" if(item.username == None) else item.username
-            item.email =  "" if(item.email == None) else item.email
-            item.requested_port_date =  "" if(item.requested_port_date == None) else item.requested_port_date
-            item.number =  "" if(item.number == None) else item.number
-            item.texting =  "" if(item.texting == None) else item.texting
-            item.e911_address =  "" if(item.e911_address == None) else item.e911_address
-            item.e911_number =  "" if(item.e911_number == None) else item.e911_number
+            item.username = "" if(item.username == None) else item.username
+            item.email = "" if(item.email == None) else item.email
+            item.requested_port_date = "" if(item.requested_port_date == None) else item.requested_port_date
+            item.number = "" if(item.number == None) else item.number
+            item.texting = "" if(item.texting == None) else item.texting
+            item.status = switchStatus(item.status)
+            item.e911_address = "" if(item.e911_address == None) else item.e911_address
+            item.e911_number = "" if(item.e911_number == None) else item.e911_number
 
         size = request.GET.get('size', 10)
         page_number = request.GET.get('page')
@@ -71,7 +91,8 @@ def service_order_list(request):
 def service_order_delete(request, id):
     try:
         service_order = Service_Order.objects.get(id=id)
-        service_order.delete()
+        service_order.status = 2
+        service_order.save()
         messages.warning(request, 'The service order was deleted successfully!')
     except Exception as e:
         messages.warning(request, e)
@@ -93,6 +114,23 @@ def service_order_add(request):
                 )
             service_order.full_clean()
             service_order.save()
+
+            # sender = 'ginik0108@gmail.com'
+            # receivers = ['comsuper0030@gmail.com']
+            # message = """From: From Person <from@example.com>
+            # To: To Person <comsuper0030@gmail.com>
+            # Subject: SMTP email example
+
+
+            # This is a test message.
+            # """
+
+            # try:
+            #     smtpObj = smtplib.SMTP('localhost')
+            #     smtpObj.sendmail(sender, receivers, message)         
+            #     print("Successfully sent email")
+            # except Exception:
+            #     pass
         except Exception as e:
             messages.warning(request, e)
 
@@ -115,6 +153,7 @@ def service_order_update(request, id):
             service_order.e911_address = request.POST['e911_address']
             service_order.e911_number = int(request.POST['e911_number']) if request.POST['e911_number'] else None
             service_order.updated_at = datetime.datetime.now()
+            service_order.status = 1
             service_order.full_clean()
             service_order.save()
             messages.success(request, 'The service order was updated successfully!')
@@ -127,6 +166,7 @@ def service_order_update(request, id):
             'username': service_order.username,
             'email': service_order.email,
             'number': service_order.number,
+            'status': switchStatus(service_order.status),
             'requested_port_date': service_order.requested_port_date.strftime('%Y-%m-%d'),
             'e911_number': service_order.e911_number,
             'e911_address': service_order.e911_address,
